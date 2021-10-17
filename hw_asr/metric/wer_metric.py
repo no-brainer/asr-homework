fix cer and wer computation

hw_asr/metric/wer_metric.py
@@ -13,9 +13,13 @@ class ArgmaxWERMetric(BaseMetric):
         super().__init__(*args, **kwargs)
         self.text_encoder = text_encoder
 
-    def __call__(self, log_probs: Tensor, text: List[str], *args, **kwargs):
+    def __call__(self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], *args, **kwargs):
         wers = []
         predictions = torch.argmax(log_probs.cpu(), dim=-1)
+        predictions = [
+            inds[: int(ind_len)]
+            for inds, ind_len in zip(predictions, log_probs_length)
+        ]
         for log_prob_vec, target_text in zip(predictions, text):
             if hasattr(self.text_encoder, "ctc_decode"):
                 pred_text = self.text_encoder.ctc_decode(log_prob_vec)