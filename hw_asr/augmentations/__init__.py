78304c20d69850103cbaa716b28a049b7655eb6f
Polina Guseva <p.guseva00@gmail.com>
2021-10-19 15:15:38 +0300

    add random apply support for augmentations

--- hw_asr/augmentations/__init__.py
+++ hw_asr/augmentations/__init__.py
@@ -3,6 +3,7 @@ from typing import List
 
 import hw_asr.augmentations.spectrogram_augmentations
 import hw_asr.augmentations.wave_augmentations
+from hw_asr.augmentations.random_apply import RandomApply
 from hw_asr.augmentations.sequential import SequentialAugmentation
 from hw_asr.utils.parse_config import ConfigParser
 
@@ -11,16 +12,18 @@ def from_configs(configs: ConfigParser):
     wave_augs = []
     if "augmentations" in configs.config and "wave" in configs.config["augmentations"]:
         for aug_dict in configs.config["augmentations"]["wave"]:
-            wave_augs.append(
-                configs.init_obj(aug_dict, hw_asr.augmentations.wave_augmentations)
-            )
+            wave_aug = configs.init_obj(aug_dict, hw_asr.augmentations.wave_augmentations)
+            if "p" in aug_dict:
+                wave_aug = RandomApply(wave_aug, aug_dict["p"])
+            wave_augs.append(wave_aug)
 
     spec_augs = []
     if "augmentations" in configs.config and "spectrogram" in configs.config["augmentations"]:
         for aug_dict in configs.config["augmentations"]["spectrogram"]:
-            spec_augs.append(
-                configs.init_obj(aug_dict, hw_asr.augmentations.spectrogram_augmentations)
-            )
+            spec_aug = configs.init_obj(aug_dict, hw_asr.augmentations.spectrogram_augmentations)
+            if "p" in aug_dict:
+                spec_aug = RandomApply(spec_aug, aug_dict["p"])
+            spec_augs.append(spec_aug)
     return _to_function(wave_augs), _to_function(spec_augs)
 
 
