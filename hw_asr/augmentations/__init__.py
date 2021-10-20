bf3acab6a57edf5d09ee91e68d542be21dd0f7a2
Polina Guseva <p.guseva00@gmail.com>
2021-10-20 16:24:54 +0300

    fix random augmentations

--- hw_asr/augmentations/__init__.py
+++ hw_asr/augmentations/__init__.py
@@ -12,17 +12,19 @@ def from_configs(configs: ConfigParser):
     wave_augs = []
     if "augmentations" in configs.config and "wave" in configs.config["augmentations"]:
         for aug_dict in configs.config["augmentations"]["wave"]:
+            aug_prob = aug_dict.pop("aug_prob", None)
             wave_aug = configs.init_obj(aug_dict, hw_asr.augmentations.wave_augmentations)
-            if "aug_prob" in aug_dict:
-                wave_aug = RandomApply(wave_aug, aug_dict["aug_prob"])
+            if aug_prob is not None:
+                wave_aug = RandomApply(wave_aug, aug_prob)
             wave_augs.append(wave_aug)
 
     spec_augs = []
     if "augmentations" in configs.config and "spectrogram" in configs.config["augmentations"]:
         for aug_dict in configs.config["augmentations"]["spectrogram"]:
+            aug_prob = aug_dict.pop("aug_prob", None)
             spec_aug = configs.init_obj(aug_dict, hw_asr.augmentations.spectrogram_augmentations)
-            if "aug_prob" in aug_dict:
-                spec_aug = RandomApply(spec_aug, aug_dict["aug_prob"])
+            if aug_prob is not None:
+                spec_aug = RandomApply(spec_aug, aug_prob)
             spec_augs.append(spec_aug)
     return _to_function(wave_augs), _to_function(spec_augs)
 
