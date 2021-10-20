22d18b3155c2452add772ac129d1812d6445c40a
Polina Guseva <p.guseva00@gmail.com>
2021-10-20 20:53:54 +0300

    fix for wer

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -12,10 +12,12 @@ def calc_cer(target_text, predicted_text) -> float:
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    if len(target_text) == 0 and len(predicted_text) == 0:
+    target_text_words = target_text.split()
+    predicted_text_words = predicted_text.split()
+
+    if len(target_text_words) == 0 and len(predicted_text_words) == 0:
         return 0.
-    elif len(target_text) == 0:
+    elif len(target_text_words) == 0:
         return 1.
 
-    target_text_words = target_text.split()
-    return editdistance.eval(target_text_words, predicted_text.split()) / len(target_text_words)
+    return editdistance.eval(target_text_words, predicted_text_words) / len(target_text_words)
