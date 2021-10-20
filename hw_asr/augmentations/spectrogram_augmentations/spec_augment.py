da2961841e401efa388a8fe3b97c8ff73e3b0b4a
Polina Guseva <p.guseva00@gmail.com>
2021-10-21 00:33:07 +0300

    small improvement for spec_augment

--- hw_asr/augmentations/spectrogram_augmentations/spec_augment.py
+++ hw_asr/augmentations/spectrogram_augmentations/spec_augment.py
@@ -11,10 +11,10 @@ class SpecAugment(AugmentationBase):
         max_freq_mask = kwargs.get("max_freq_mask", 20)
         max_time_mask = kwargs.get("max_time_mask", 100)
 
-        self._aug = Sequential(
-            torchaudio.transforms.FrequencyMasking(max_freq_mask),
-            torchaudio.transforms.TimeMasking(max_time_mask),
-        )
+        self.freq_mask = torchaudio.transforms.FrequencyMasking(max_freq_mask)
+        self.time_mask = torchaudio.transforms.TimeMasking(max_time_mask)
 
     def __call__(self, data: torch.Tensor):
-        return self._aug(data)
+        val = data.mean()
+        data = self.freq_mask(data, val)
+        return self.time_mask(data, val)
